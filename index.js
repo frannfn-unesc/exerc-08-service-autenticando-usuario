9c9628062749cb57bdf6435aea65fecc988f3431
Francine Nunes <frann.fn@unesc.net>
2021-06-17 16:01:31 -0300

    Ajustado

--- index.js
+++ index.js
@@ -2,14 +2,37 @@ const express = require('express')
 const app = express()
 const PORTA = 8080
 
+app.listen(PORTA, function(){
+    console.log(`Servidor iniciado na porta ${PORTA}`)
+})
+
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 
 app.use('/', function(req, res){
-    console.log("Olá Mundo")
-    res.sendFile("./public/login.html")
+    res.sendFile(__dirname + "/public/login.html")
 })
 
-app.listen(PORTA, function(){
-    console.log(`Servidor iniciado na porta ${PORTA}`)
+app.use('/login', function(req, res){
+    res.sendFile(__dirname + "/public/login.html")
 })
+
+app.use("/profile", function(req, res) {
+    res.sendFile(__dirname + "/public/profile.html");
+})
+
+app.use('/auth', function(req, res){
+    var usuario = "Daiana"
+    var senha = "12345"
+
+    if(req.body.usuario == usuario && req.body.senha == senha){
+        res.redirect(__dirname + "/public/profile.html")
+    } else {
+        res.redirect(__dirname + "/public/login.html")
+    }
+})
+
+app.use(function(req, res){
+    res.status(404);
+    res.sendFile(__dirname + "/public/erro.html");
+});
